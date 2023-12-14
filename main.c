a4024da0d138748e934716e94ac93586f805aa4c
08168005790 <amehene2017@gmail.com>
2023-12-14 10:31:56 -0800

    main.c

--- main.c
+++ main.c
@@ -1,46 +1,52 @@
+#define _POSIX_C_SOURCE 200809L
 #include "monty.h"
-#include <stdio.h>
-bus_t bus = {NULL, NULL, NULL, 0};
+monty_inst instance_vars = {NULL, NULL, NULL};
+
 /**
-* main - monty code interpreter
-* @argc: number of arguments
-* @argv: monty file location
-* Return: 0 on success
-*/
+ * main - Entry point, interpreter for Monty bytecodes files
+ *
+ * @argc: argument count
+ * @argv: argument vector, monty file location
+ *
+ * Return: 0 (on Success)
+ */
+
 int main(int argc, char *argv[])
 {
-	char *content;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read = 1;
 	FILE *file;
-	size_t size = 0;
-	ssize_t read_line = 1;
+	unsigned int line_number = 0;
 	stack_t *stack = NULL;
-	unsigned int counter = 0;
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
+
 	file = fopen(argv[1], "r");
-	bus.file = file;
-	if (!file)
+
+	if (file == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	while (read_line > 0)
+	instance_vars.file = file;
+
+	while (read > 0)
 	{
-		content = NULL;
-		read_line = getline(&content, &size, file);
-		bus.content = content;
-		counter++;
-		if (read_line > 0)
-		{
-			execute(content, &stack, counter, file);
-		}
-		free(content);
+		read = getline(&line, &len, file);
+		instance_vars.line = line;
+		line_number++;
+		if (read != -1)
+			execute(&stack, line_number, line, file);
 	}
-	free_stack(stack);
+
+	free(line);
+	free_struct(stack);
 	fclose(file);
-return (0);
+
+	return (0);
 }
